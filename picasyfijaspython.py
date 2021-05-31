c017eddd8f3bfe4aa53659c89f78ac0e8a7b5555
alex0325180 <85022420+alex0325180@users.noreply.github.com>
2021-05-31 17:43:05 -0500

    Update picasyfijaspython.py
    -	Un bucle while permite repetir la ejecución de un grupo de instrucciones mientras se cumpla una condición (es decir, mientras la condición tenga el valor True).
    -	En Python, la sentencia if se utiliza para ejecutar un bloque de código si, y solo si, se cumple una determinada condición. Por tanto, if es usado para la toma de decisiones. Es decir, solo si condición se evalúa a True, se ejecutarán las sentencias que forman parte de bloque de código 
    -	Opcionalmente, puedes agregar una respuesta else que se ejecutará si la condición es false ejemplo
    -	y = 3
    -	if y > 4:
    -	  print("¡No voy a imprimir!") #esta sentencia no se ejecuta
    -	else:
    -	  print("¡La condición no era verdadera!") #esta sentencia se ejecuta

--- picasyfijaspython.py
+++ picasyfijaspython.py
@@ -24,3 +24,65 @@ def __validar_longitud__(numero,longitud): #Se revisa la longitud del numero
         return __validar_numero__(numero)
     else:
         return False
+def __puntaje__(a):
+    f = open("archivo.txt","r")
+    menor=1000
+    puntajes = []
+    for i in f:
+        i = i.rstrip()
+        if not i.startswith(a):
+            continue
+        score = i.split(",")
+        puntajes.append(score)
+        puntaje = int(score[1])
+        if(menor > puntaje):
+            menor = puntaje
+    for j in puntajes:
+        menor=str(menor)
+        if (j[1]==menor):
+            print ("El mejor en ",a," digitos es:",j[3]," con ",j[1]," intentos")
+    
+    
+
+
+def __comprobar__(
+    intentos,numero,
+    entrada): #Comienza el juego de picas y fijas
+    resultado = ""
+    for i in range(intentos):
+            fijas = 0
+            picas = 0
+            if intentos == 1:
+                resultado = "perdiste "
+                print(resultado)
+                intent = 0
+                return [len(numero),intent, resultado]
+            else:
+                for i in range(len(entrada)):
+                    if numero[i] == entrada[i]:
+                        fijas = fijas + 1
+                    elif numero[i] in entrada:
+                        picas = picas + 1
+                if fijas == len(entrada):
+                    resultado = "ganaste"
+                    print(resultado)
+                    intent = 11 - intentos
+                    return [len(numero),intent, resultado]
+                intentos= intentos-1
+                print("tienes ", fijas, "fijas y tienes ", picas, "picas\n intentos restantes: "
+                ,intentos)
+                while True:
+                    entrada = [int(x) for x in input("Ingrese un numero: ")]
+                    if __validar_longitud__(entrada,len(numero)) == True:
+                        break           
+                    try:
+                        e = __validar_longitud__(entrada,len(numero))
+                        if e == False:
+                            quit()
+                    except:
+                        print("Ingrese un numero valido")
+                        continue
+                
+    
+cifras = 0
+intentos = 0
