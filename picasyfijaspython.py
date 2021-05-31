761b804eb0b7efe65e734e0b463ab6dd8c2ac990
alex0325180 <85022420+alex0325180@users.noreply.github.com>
2021-05-31 17:37:44 -0500

    Update picasyfijaspython.py
    -	Las funciones en Python se crean usando la palabra clave def, seguida de un nombre de función y parámetros de función entre paréntesis.
    -	Un bucle while permite repetir la ejecución de un grupo de instrucciones mientras se cumpla una condición (es decir, mientras la condición tenga el valor True).

--- picasyfijaspython.py
+++ picasyfijaspython.py
@@ -11,4 +11,11 @@ def __generar_secreto__(cantidad): #Se genera un numero aleatorio sin repeticion
             break
     return secreto
   
-  
+def __validar_numero__(numero): #Se valida que cada digito del numero sea diferente
+    if len(numero) == 1:
+        return True
+    else:
+        if numero[0] in numero[1:]:
+            return False
+        else: 
+            return __validar_numero__(numero[1:])
