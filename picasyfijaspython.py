636925012e231b69e81f31615693fd965f7c5ed3
hdtaberner <73736095+hdtaberner@users.noreply.github.com>
2021-05-31 17:41:40 -0500

    Update picasyfijaspython.py
    -	es una sentencia import. Recuerda, las sentencias son instrucciones que realizan alguna acción, pero no son evaluadas a un valor como las expresiones. Ya has visto sentencias antes: las sentencias de asignación almacenan un valor en una variable.
    -	random- Este módulo implementa generadores de números pseudoaleatorios para varias distribuciones.
    -	Las funciones en Python se crean usando la palabra clave def, seguida de un nombre de función y parámetros de función entre paréntesis.

--- picasyfijaspython.py
+++ picasyfijaspython.py
@@ -1,4 +1,3 @@
-#Juego de picas y Fijas Python
 from random import randint
 
 def __generar_secreto__(cantidad): #Se genera un numero aleatorio sin repeticion
@@ -10,5 +9,18 @@ def __generar_secreto__(cantidad): #Se genera un numero aleatorio sin repeticion
         if len(secreto) == cantidad:
             break
     return secreto
-  
-  
+
+def __validar_numero__(numero): #Se valida que cada digito del numero sea diferente
+    if len(numero) == 1:
+        return True
+    else:
+        if numero[0] in numero[1:]:
+            return False
+        else: 
+            return __validar_numero__(numero[1:])
+
+def __validar_longitud__(numero,longitud): #Se revisa la longitud del numero
+    if len(numero) == longitud:
+        return __validar_numero__(numero)
+    else:
+        return False
